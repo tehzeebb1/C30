a5dec96fa69e26e9d14e066fdd2669f3d3814dcb
tehzeebb1 <tehzeebb1@educbe.ca>
2020-10-19 19:49:22 -0600

    Vanishing Effect

--- Pig.js
+++ Pig.js
@@ -2,6 +2,23 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.Visibilty = 255;
   }
-
+display()
+{
+ if(this.body.speed<3) 
+ {
+  console.log(this.body.speed)
+  super.display()
+ }
+ else
+ {
+  World.remove(world, this.body);
+  push();
+  this.Visibilty = this.Visiblity-5;
+  tint (255, this.Visibilty);
+  image(this.image,this.body.position.x,this.body.position.y,50,50);
+  pop();
+ }
+}
 };
